3fe0da7ad28ad8ad72f9a1b5a8599df1cede000b
cstdout <151808195+cstdout@users.noreply.github.com>
2024-10-25 01:07:53 -0700

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+int main() {
+    srand(time(0)); 
+    int n;
+    std::cin >> n;    
+
+   int *arr = new int[n];
+    
+   for(int i = 0; i < n; ++i)
+   {
+       arr[i] = rand() % 201 - 100;
+       std::cout << arr[i] << ' ';
+   }
+   std::cout << std::endl;
+   int *uniqueElems = new int[n];
+   int uniqueElemCount = 0;
+   bool exists;
+   for(int i = 0; i < n; ++i)
+   {
+       exists = false;
+       for(int j = 0; !exists && j < uniqueElemCount; ++j)
+       {
+           exists = (arr[i] == uniqueElems[j]);
+       }
+       if (!exists)
+       {
+           
+           uniqueElems[uniqueElemCount] = arr[i];
+           ++uniqueElemCount;
+       }
+   }
+   for(int i = 0; i < uniqueElemCount; ++i)
+   {
+       std::cout << uniqueElems[i] << ' ';
+   }
+
+    return 0;
+}
